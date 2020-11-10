b7311cab33d576a6e4a25e963abd5bfbfeb8967c
ISSE-AILab <67357951+ISSE-AILab@users.noreply.github.com>
2020-11-10 15:53:31 +0800

    Update train.py

--- train.py
+++ train.py
@@ -10,7 +10,6 @@ config = tf.ConfigProto()
 config.gpu_options.per_process_gpu_memory_fraction = 0.5
 config.gpu_options.allow_growth = True
 sess = tf.Session(config = config)
-
 def train():
     '''
     Preprocessing for dataset
